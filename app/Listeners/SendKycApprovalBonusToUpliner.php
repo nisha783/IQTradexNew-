86881f37c07d4aca6d59bbe2734c0ea00dbf67d1
nisha783 <hm6980097@gmail.com>
2024-12-09 23:38:23 +0500

    pdf

--- app/Listeners/SendKycApprovalBonusToUpliner.php
+++ app/Listeners/SendKycApprovalBonusToUpliner.php
@@ -24,57 +24,52 @@ class SendKycApprovalBonusToUpliner implements ShouldQueue
     public function handle(KycUserVerified $event): void
     {
         info("SendKycApprovalBonusToUpliner: " . $event->user->name);
-        // checking if this user has valid upliner
+
+        // Fetch the user
         $user = $event->user;
 
-        // sending bonus to this user
-        $bonus_amount = $user->settings('kyc_approval_bonus');
+        // Sending bonus to this user
+        $bonus_amount = $user->settings('approval_bonus');
         if ($user->userPlans->count() == 0) {
             $transaction = Transaction::firstOrCreate([
                 'user_id' => $user->id,
                 'amount' => $bonus_amount,
                 'status' => 'approved',
-                'type' => 'kyc bonus',
+                'type' => 'approval bonus',
                 'sum' => true,
                 'payment_status' => false,
-                'reference' => 'Kyc Approval Bonus',
+                'reference' => 'Approval Bonus',
             ]);
         }
 
+        // Check if the user has an upliner
         if ($user->upliner) {
             $upliner = $user->upliner;
-            $bonus_amount = $user->settings('kyc_approval_bonus_for_upliner');
-            info('Bonus amount: ' . $bonus_amount);
-            // checking if this user has already active plan
+            $upliner_bonus_amount = $user->settings('approval_bonus_for_upliner');
+            info('Bonus amount for upliner: ' . $upliner_bonus_amount);
+
+            // Checking if upliner has no active plan
             if ($upliner->userPlans->count() == 0) {
                 info("Upliner {$upliner->name} has no active plan");
-                // adding commission
-                // checking if the upline KYC is approved
-                if ($upliner->kyc && $upliner->kyc->status == 'approved') {
-                    info('Upliner KYC is approved');
 
-                    // checking if this user already get kyc bonus more then 8 time
-                    info($upliner->transactions()->where('type', 'kyc bonus')->where('additional_type', 'kyc bonus from downline')->count());
-                    if ($upliner->transactions()->where('type', 'kyc bonus')->where('additional_type', 'kyc bonus from downline')->count() < 8) {
-                        $transaction = Transaction::firstOrCreate([
-                            'user_id' => $upliner->id,
-                            'amount' => $bonus_amount,
-                            'status' => 'approved',
-                            'type' => 'kyc bonus',
-                            'additional_type' => 'kyc bonus from downline',
-                            'payment_status' => false,
-                            'sum' => true,
-                            'reference' => 'Bonus from ' . $user->username . ' on KYC approval',
-                        ]);
-                        info('Transaction added: ' . $transaction->id);
-                    } else {
-                        info('User already get kyc bonus more then 8 time');
-                    }
+                // Check if upliner qualifies for bonus
+                if ($upliner->transactions()->where('type', 'approval bonus')->where('additional_type', 'bonus from downline')->count() < 8) {
+                    $transaction = Transaction::firstOrCreate([
+                        'user_id' => $upliner->id,
+                        'amount' => $upliner_bonus_amount,
+                        'status' => 'approved',
+                        'type' => 'approval bonus',
+                        'additional_type' => 'bonus from downline',
+                        'payment_status' => false,
+                        'sum' => true,
+                        'reference' => 'Bonus from ' . $user->username . ' on approval',
+                    ]);
+                    info('Transaction added: ' . $transaction->id);
                 } else {
-                    info('Upliner KYC is not approved');
+                    info('Upliner already received approval bonus more than 8 times');
                 }
             } else {
-                info('User not qualified for kyc bonus: because has active plan');
+                info('Upliner not qualified for approval bonus: has active plan');
             }
         }
     }
